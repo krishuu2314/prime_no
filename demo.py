f28f80542bc1b30c1e69667f259e68b6095ca04c
Your Name <you@example.com>
2026-03-03 22:24:53 +0530

    Refined version of the program

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
-n= int(input("Enter any number: "))
-i=2
-f=0
-while i<n:
-    if n%i==0:
-        f=1
-    i+=1
-if f==0:
-    print("The number is prime")
-else:
+n = int(input("Enter any number: "))
+
+if n <= 1:
     print("The number is not prime")
+else:
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            print("The number is not prime")
+            break
+        i += 1
+    else:
+        print("The number is prime")
